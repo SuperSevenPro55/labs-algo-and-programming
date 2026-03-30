c797baaf36bdebc79945bc91aafb1c159fd1db22
Roman <romanplotskiy@gmail.com>
2026-03-30 22:50:07 +0600

    Lab 4.2: Completed PriorityQueueImpl.java

--- src/main/java/ru/Labs/ui/labs/Lab_4_2.java
+++ src/main/java/ru/Labs/ui/labs/Lab_4_2.java
@@ -14,39 +14,9 @@ public class Lab_4_2 implements LabRunner {
 
     @Override
     public void start() {
-        StringBuilder sb = new StringBuilder();
+        System.out.println(MessageManager.get("menu.lab4.item.2.commands"));
         PriorityQueue<Integer> queue = new PriorityQueueImpl();
 
-        System.out.println(MessageManager.get("menu.lab4.item.2.commands"));
 
-        runCommandLoop(scanner, queue, sb);
-    }
-
-    private static void runCommandLoop(Scanner scanner, PriorityQueue<Integer> queue, StringBuilder sb) {
-        while (true) {
-            String command = scanner.next();
-
-            if (command.equals("exit")) {
-                break;
-            }
-
-            processCommand(scanner, command, queue, sb);
-        }
-    }
-
-    private static void processCommand(Scanner scanner, String command, PriorityQueue<Integer> queue, StringBuilder sb) {
-        switch (command) {
-            case "enqueue" -> {
-                queue.enqueue(0);
-            }
-            case "dequeue-max" -> {
-                queue.dequeueMax();
-            }
-            case "inc" -> {
-                queue.increment(0, 1);
-            }
-
-            default -> System.out.println(MessageManager.get("error.invalid_input"));
-        }
     }
 }
