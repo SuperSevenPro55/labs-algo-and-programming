ad7dbe35e879d6d0be78841c450206eb6bdcd3bc
Roman <romanplotskiy@gmail.com>
2026-04-25 13:06:06 +0600

    Added another controller test for Lab_10_1

--- src/test/java/ru/Labs/ui/labs/TestLab_10_1.java
+++ src/test/java/ru/Labs/ui/labs/TestLab_10_1.java
@@ -44,4 +44,39 @@ public class TestLab_10_1 {
         assertTrue(consoleOutput.contains("3 6 7 9"));
         assertTrue(consoleOutput.contains("5"));
     }
+
+    @Test
+    public void controller_correctInputRandomized_correctOutput() {
+        String userInput = """
+                10
+                7
+                3 7
+                1 0
+                7 9
+                8 1
+                1 4
+                0 2
+                3 6
+                """;
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
+        Scanner scanner = new Scanner(inputStream);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        try {
+            LabRunner lab = new Lab_10_1(scanner);
+            lab.start();
+        } finally {
+            System.setOut(printStream);
+        }
+
+        String consoleOutput = outputStream.toString().replace("\r", "");
+
+        assertTrue(consoleOutput.contains("0 1 2 4 8"));
+        assertTrue(consoleOutput.contains("3 6 7 9"));
+        assertTrue(consoleOutput.contains("5"));
+    }
 }
